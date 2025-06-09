cb23111049649e581d2d0a7c5f51d7cd6e4b86f3
Zeliha İnan <zelihainann@gmail.com>
2025-06-09 15:58:26 +0300

    bugfix

--- robot.h
+++ robot.h
@@ -28,19 +28,16 @@ public:
         return glm::distance(position, target) < threshold;
     }
 
-
     void draw(Shader& shader, float armAngle) {
-        // === GÖVDE ===
         glm::mat4 bodyMat = glm::mat4(1.0f);
         bodyMat = glm::translate(bodyMat, position + glm::vec3(0.0f, 0.6f, 0.0f));
         bodyMat = glm::rotate(bodyMat, glm::radians(rotationY), glm::vec3(0.0f, 1.0f, 0.0f));
         bodyMat = glm::scale(bodyMat, glm::vec3(0.5f));
         shader.setMat4("model", bodyMat);
-        glUniform1i(glGetUniformLocation(shader.ID, "useTexture"), false);
+        glUniform1i(glGetUniformLocation(shader.ID, "useTexture"), true);
         glUniform3f(glGetUniformLocation(shader.ID, "objectColor"), 0.6f, 0.6f, 0.6f);
         body.Draw(shader);
 
-        // === KOL (gövdeye bağlı) ===
         glm::mat4 armMat = glm::mat4(1.0f);
 
         armMat = glm::translate(armMat, glm::vec3(-0.030f, -0.015f, -0.02f));
